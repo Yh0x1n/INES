6faa7548b76eb9495e202e5f3076293c36b4f468
Yh0x1n01~ <yhoxrossell508@gmail.com>
2022-04-12 04:43:28 -0430

    testing with methods and random user ID generation

--- main.py
+++ main.py
@@ -2,12 +2,25 @@
 #by Yhoxin Rossell, Hernán Guerrero and Douglas Socorro
 
 from fastapi import FastAPI
-import random
+from lib2to3.pygram import Symbols
+import numbers, random
 
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lower = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
+Symbols = '¿?¡!$%&#-_+-'
+all = upper + lower + numbers + Symbols
+len = 10
+user = ''.join(random.sample(all, len))
+nick = "random user"
 
+    
 mainsite = FastAPI()
 
-@mainsite.get("/")
-
+@mainsite.get("/main")
 def testmsg():
-    return "Hello! This is the first step of project INES. ^^"
+    return "Hello! This is the first step for project INES. ^^"
+
+@mainsite.get("/users-ID")
+def usuario():
+    return f"Hello, {nick}! your user ID is {user}."
