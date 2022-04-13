5672cc201c6410fb2e63daacf23ab5569271627f
Yh0x1n01~ <yhoxrossell508@gmail.com>
2022-04-12 23:27:42 -0430

    Added test class for user ID generation

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 #INES
 #by Yhoxin Rossell, Hernán Guerrero and Douglas Socorro
 
+from pydantic import BaseModel
 from fastapi import FastAPI
-from lib2to3.pygram import Symbols
-import numbers, random
+import random
 
-upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-lower = 'abcdefghijklmnopqrstuvwxyz'
-numbers = '0123456789'
-Symbols = '¿?¡!$%&#-_+-'
-all = upper + lower + numbers + Symbols
-len = 10
-user = ''.join(random.sample(all, len))
-nick = "random user"
+class user_generator(): # Class for user ID generator
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    numbers = '0123456789'
+    Symbols = '¿?¡!$%&#-_+\/<ñ>'
+    all = upper + lower + numbers + Symbols
+    len = 10
+    user = ''.join(random.sample(all, len))
 
+class userID(BaseModel, user_generator): # Class for username and ID parameters
+    nick : str
+    id = user_generator.user
     
+# Main app section
 mainsite = FastAPI()
 
-@mainsite.get("/main")
+@mainsite.get("/main") # Test message
 def testmsg():
     return "Hello! This is the first step for project INES. ^^"
 
-@mainsite.get("/users-ID")
-def usuario():
-    return f"Hello, {nick}! your user ID is {user}."
+@mainsite.post("/users-ID") # Response body for username and ID
+def usuario(userid: userID):
+    return f"Hello, {userid.nick}! your user ID is {userid.id}."
