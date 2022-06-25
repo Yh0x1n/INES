eda4499db4858aaf11a9a4e56d460e5623a0b488
Douglas Socorro <douglassocorro.work@gmail.com>
2022-06-25 19:28:36 -0400

    Remove atributes from users table

--- src/db_schemas.sql
+++ src/db_schemas.sql
@@ -12,8 +12,8 @@ CREATE TABLE if NOT EXISTS usuarios (
   apellido2 VARCHAR (50) NULL,
   apellido3 VARCHAR (50) NULL,
   cedula VARCHAR (15) NOT NULL
-  nacimiento DATE NOT NULL NULL,
-  es_admin BINARY (1) NOT NULL
+  #nacimiento DATE NOT NULL NULL,
+  #es_admin BINARY (1) NOT NULL
 );
 
 CREATE TABLE if NOT EXISTS instrumentos (
